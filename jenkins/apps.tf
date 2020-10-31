1dba07c95f4dce230cf4a74a77d118b6e7870309
ueong <dnfod@me.com>
2020-10-31 13:45:10 +0900

    helm chart version fixed

--- jenkins/apps.tf
+++ jenkins/apps.tf
@@ -6,7 +6,7 @@ resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://kubernetes-charts.storage.googleapis.com"
   chart      = "jenkins"
-  version    = "2.6.4"
+  version    = "2.5.4"
   timeout    = 1200
 
   values = [data.local_file.helm_chart_values.content]

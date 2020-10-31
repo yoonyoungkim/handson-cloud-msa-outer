dda045b411966266fc901587596b1fcebf263f04
ueong <dnfod@me.com>
2020-10-31 13:39:04 +0900

    helm chart added

---
+++ jenkins/apps.tf
@@ -0,0 +1,17 @@
+data "local_file" "helm_chart_values" {
+  filename = "${path.module}/values.yaml"
+}
+
+resource "helm_release" "jenkins" {
+  name       = "jenkins"
+  repository = "https://kubernetes-charts.storage.googleapis.com"
+  chart      = "jenkins"
+  version    = "2.6.4"
+  timeout    = 1200
+
+  values = [data.local_file.helm_chart_values.content]
+
+  depends_on = [
+    kubernetes_secret.gh-secrets,
+  ]
+}

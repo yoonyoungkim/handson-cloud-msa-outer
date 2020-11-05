52cc916167a8d85d8cc5a33a2b8024900fb24842
ueong <dnfod@me.com>
2020-11-05 19:53:58 +0900

    repo fixed

--- environments/dev/apps.tf
+++ environments/dev/apps.tf
@@ -1,7 +1,7 @@
 resource "helm_release" "nats-operator" {
   name       = "nats-operator"
   repository = "https://geek-cookbook.github.io/charts"
-  chart      = "geek-cookbook/nats-operator"
+  chart      = "nats-operator"
   version    = "0.1.3"
   timeout    = 1200
 }
@@ -9,7 +9,7 @@ resource "helm_release" "nats-operator" {
 resource "helm_release" "nginx-ingress-controller" {
   name       = "nginx-ingress-controller"
   repository = "https://helm.nginx.com/stable"
-  chart      = "stable/nginx-ingress"
+  chart      = "nginx-ingress"
   version    = "0.7.0"
   timeout    = 1200
 }

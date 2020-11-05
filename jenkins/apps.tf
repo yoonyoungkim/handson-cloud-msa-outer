63d7c341ee0f52cf9a1847ef87ee00c4ba584748
ueong <dnfod@me.com>
2020-11-05 11:19:22 +0900

    argocd

--- jenkins/apps.tf
+++ jenkins/apps.tf
@@ -2,6 +2,10 @@ data "local_file" "helm_chart_values" {
   filename = "${path.module}/values.yaml"
 }
 
+data "local_file" "argo_cd_values" {
+  filename = "${path.module}/argo-cd-values.yaml"
+}
+
 resource "helm_release" "jenkins" {
   name       = "jenkins"
   repository = "https://kubernetes-charts.storage.googleapis.com"
@@ -14,5 +18,18 @@ resource "helm_release" "jenkins" {
   depends_on = [
     kubernetes_secret.gh-secrets,
     kubernetes_secret.jenkins-secrets,
+  ]
+}
+
+resource "helm_release" "argo-cd" {
+  name       = "argo-cd"
+  repository = "https://argoproj.github.io/argo-helm"
+  chart      = "argo-cd"
+  version    = "2.9.5"
+  timeout    = 1200
+
+  values     = [data.local_file.argo_cd_values.content]
+  depends_on = [
+    kubernetes_secret.gh-secrets,
   ]
 }

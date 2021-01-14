0a30c898a9c2cf5260639b992ae63c512c2e60e0
Wooyoung Jung <dnfod@me.com>
2021-01-14 15:20:13 +0900

    "https://www.googleapis.com/auth/trace.append" added

--- environments/dev/gke.tf
+++ environments/dev/gke.tf
@@ -32,6 +32,7 @@ resource "google_container_node_pool" "primary_nodes" {
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/trace.append"
     ]
 
     labels = {
